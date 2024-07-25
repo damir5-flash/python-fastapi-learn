a2ef15228a972026ca517cbaf2204b2fcc754dd0
Дамир <rayimbekdamir5@gmail.com>
2024-07-25 19:28:39 +0500

    init async alembic+black

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -13,7 +13,7 @@ from core.models.db_helper import db_helper
 async def lifespan(apps: FastAPI):
     # start-up
     async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+        await conn.run_sync(Base.metadata.drop_all)
     yield
     # shut-down
     db_helper.despose()
